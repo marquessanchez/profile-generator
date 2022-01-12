3c3024861a501580fba985622bfe5550abc759fa
mrican217@gmail.com <mrican217@gmail.com>
2022-01-12 00:11:17 -0500

    questions asked to get employee. Left off trying to loop through question.Employee in index.js

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const fs = require('fs');
 const { prompt } = require('inquirer');
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 
+var team = ['Manager','Engineer','Engineer','Engineer','Intern'];
 
-// const questions = [
-//   {
-//     type:'input',
-//     name: 'team member',
-//     message: 'What team member are you adding?'
-// }]
+const questions = {
+  'Employee': ['name','id','email'],
+  'Manager': ['officeNumber'],
+  'Engineer': ['GitHub'],
+  'Intern': ['school']
+};
+
+prompt({
+  'type': 'list',
+  'name':'employee',
+  'message': 'What position would you like to fill?',
+  'choices': ['Manager','Engineer','Intern']
+}).then(role=>{
+  prompt(
+    questions.Employee.forEach(item => {
+      {
+      'type':'input',
+      'name': item,
+      'message': `What is your ${item}`
+      }
+    }))
+})
